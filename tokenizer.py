0a949ec5b4c518a502dc0b97a066ed7dbc20329c
nayeon7lee <nayeon7lee@gmail.com>
2017-03-08 23:50:36 +0800

    resolved Issue #2

--- tokenizer.py
+++ tokenizer.py
@@ -1,11 +1,21 @@
 import string
 import re
 
+#filter_non_alphanum_punc
+def filter_non_alphanum_punc(inputStr):
+    emoji_pattern = re.compile("["
+            u"\U0001F600-\U0001F64F"  # emoticons
+            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+            u"\U0001F680-\U0001F6FF"  # transport & map symbols
+            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', inputStr)
 
 # to_words
-# options = {"padStart": integer, "padEnd": integer, "includePunct":boolean}
+# options = {"padStart": integer, "padEnd": integer, "includePunct":boolean, "filterNonAlphaNumPun":boolean}
 def to_words(inputStr, options={}):
     includePunct = True if "includePunct" not in options else options["includePunct"]
+    filterNonAlphaNumPun = True if "filterNonAlphaNumPun" not in options else options["filterNonAlphaNumPun"]
     padStart = 0 if "padStart" not in options else options["padStart"]
     padEnd = 0 if "padEnd" not in options else options["padEnd"]
 
@@ -13,6 +23,10 @@ def to_words(inputStr, options={}):
 
     #append <start> tokens if any
     word_list.extend(['<start>'] * padStart)
+
+    # apply additional filtering to inputStr if necessary
+    if filterNonAlphaNumPun:
+        inputStr = filter_non_alphanum_punc(inputStr)
 
     processedInputStr = ''
     if includePunct:
@@ -28,12 +42,16 @@ def to_words(inputStr, options={}):
     return word_list
 
 # to_chars
-# options = {"includeSpace": boolean, "includePunct": boolean}
+# options = {"includeSpace": boolean, "includePunct": boolean, "filterNonAlphaNumPun":boolean}
 def to_chars(inputStr, options={}):
+    filterNonAlphaNumPun = True if "filterNonAlphaNumPun" not in options else options["filterNonAlphaNumPun"]
     includePunct = True if "includePunct" not in options else options["includePunct"]
     includeSpace = False if "includeSpace" not in options else options["includeSpace"]
 
     char_list = [];
+
+    if filterNonAlphaNumPun:
+        inputStr = filter_non_alphanum_punc(inputStr)
 
     puncHandledStr = inputStr
     if not includePunct:
@@ -51,11 +69,15 @@ def to_chars(inputStr, options={}):
 
 # testing
 if __name__ == '__main__':
-    word = "This is python testing on jupyter. This is Nayeon's Testing page? hoho! Haha!!!"
-    char = 'anaconda is nice!!! Wow. a'
+    word = "This is python testing on jupyter. This is Nayeon's Testing page? hoho! Haha!!!" + u'\U0001f602'
+    char = 'anaconda is nice!!! Wow. a' + u'\U0001f602'
+
+    print(word)
 
     print(to_words(word))
+    print(to_words(word, options={"filterNonAlphaNumPun": False }))
     print(to_words(word, options={"padStart": 1, "padEnd": 1}))
     print(to_chars(char))
+    print(to_chars(char, options={"filterNonAlphaNumPun": False}))
     print(to_chars(char, options={"includePunct": False}))
     print('end...')
