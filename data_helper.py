ab56657991721753850b10fdf7966732ef4f61ab
jihopark <lastspurt17@gmail.com>
2017-07-17 11:33:27 +0800

    new evaluation

--- data_helper.py
+++ data_helper.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 from keras.utils import to_categorical
 
@@ -12,9 +13,9 @@ def load_waasem(path):
                                            % (split, label, "waasem"))
     return data_w
 
-def make_into_categorical_binary(original_data, labels):
+def make_into_categorical(original_data, labels):
     data = {}
-    for split in splits:
+    for split in original_data.keys():
         x = "x_" + split
         y = "y_" + split
         data[x] = None
@@ -28,7 +29,7 @@ def make_into_categorical_binary(original_data, labels):
             print("split:%s, label:%s, data shape:%s" %
                   (split, label, str(data[x].shape)))
             data[y] += [i for _ in range(len(_data))]
-        data[y] = to_categorical(data[y], num_classes=2)
+        data[y] = to_categorical(data[y], num_classes=len(labels))
 
     return data
 
@@ -64,7 +65,75 @@ def load_abusive_binary(_type, include_davidson=True):
             data_w[split]["abusive"] = np.vstack((data_w[split]["sexism"], data_w[split]["racism"]))
 
     labels = ["none", "abusive"]
-    return make_into_categorical_binary(data_w, labels), labels
+    return make_into_categorical(data_w, labels), labels
+
+def load_mixed_testset(_type):
+    if _type in ["char", "word"]:
+        path = "./data/%s_outputs/" % _type
+    else:
+        raise ValueError("wrong type")
+
+    data_w = load_waasem(path)
+
+    data_d = {}
+    data_d["test"] = {}
+    for label in ["none", "abusive"]:
+        data_d["test"][label] = np.load(path + "%s_%s_%s.npy"
+                                       % ("test", label, "davidson"))
+    sample_none_n = len(data_w["test"]["none"])
+    sample_abusive_n = len(data_w["test"]["sexism"]) + len(data_w["test"]["racism"])
+
+    print("sampling %s none and %s abusive label from davidson" %
+            (sample_none_n, sample_abusive_n))
+
+    none_idx = np.random.randint(len(data_d["test"]["none"]), size=sample_none_n)
+    abusive_idx = np.random.randint(len(data_d["test"]["abusive"]),
+            size=sample_abusive_n)
+
+
+    data_d["test"]["none"] = data_d["test"]["none"][none_idx, :]
+    print(data_d["test"]["none"].shape)
+
+    data_d["test"]["abusive"] = data_d["test"]["abusive"][abusive_idx, :]
+    print(data_d["test"]["abusive"].shape)
+
+    for label in ["none", "sexism", "racism"]:
+        if label == "none":
+            data_d["test"]["none"] = np.vstack((data_d["test"]["none"],
+                                        data_w["test"]["none"]))
+        else:
+            data_d["test"]["abusive"] = np.vstack((data_d["test"]["abusive"],
+                                           data_w["test"][label]))
+
+    labels = ["none", "abusive"]
+    return make_into_categorical(data_d, labels), labels
+
+def load_multiclass(_type, include_davidson=True):
+    if _type in ["char", "word"]:
+        path = "./data/%s_outputs/" % _type
+    else:
+        raise ValueError("wrong type")
+
+    data_w = load_waasem(path)
+
+    if include_davidson:
+        data_d = {}
+        for split in splits:
+            data_d[split] = {}
+            for label in ["none", "abusive"]:
+                data_d[split][label] = np.load(path + "%s_%s_%s.npy"
+                                               % (split, label, "davidson"))
+
+        # merge
+        for split in splits:
+            for label in ["none", "sexism", "racism"]:
+                if label == "none":
+                    data_w[split]["none"] = np.vstack((data_w[split]["none"],
+                                                       data_d[split]["none"]))
+
+    labels = data_w["train"].keys()
+    return make_into_categorical(data_w, labels), labels
+
 
 def load_sexism_racism_binary(_type):
     if _type in ["char", "word"]:
@@ -75,7 +144,7 @@ def load_sexism_racism_binary(_type):
     data_w = load_waasem(path)
     labels = ["sexism", "racism"]
 
-    return make_into_categorical_binary(data_w, labels), labels
+    return make_into_categorical(data_w, labels), labels
 
 
 
